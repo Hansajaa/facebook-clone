c879169a12f2f0dc25ce69562cb3a2be9a9f9885
Hansajaa <nilanahansaja21@gmail.com>
2024-02-18 09:25:44 +0530

    Add Title Component and Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login.jsx'
+import Title from './components/Title'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <Title></Title>
+        </div>
+        <div className="col-6">
+          <Login></Login>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title(){
+    return(
+        <div className="container">
+                <img className="mb-0" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" width={400} height={300} alt="" />
+                <h2 className="mt-0">Facebook helps you connect and share with the people in your life.</h2>
+        </div>
+    )
+}
